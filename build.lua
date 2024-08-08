88c59e24d82302982120fe555fa141c3365144bd
Clea F. Rees <ReesC21@cardiff.ac.uk>
2024-08-08 16:35:01 +0100

    add content to readme, split out taggint from build.lua and add manifest script, remove spurious 2010 from dtx/ins

--- build.lua
+++ build.lua
@@ -1,4 +1,4 @@
--- $Id: build.lua 10199 2024-08-08 02:53:42Z cfrees $
+-- $Id: build.lua 10202 2024-08-08 15:33:47Z cfrees $
 -- Build configuration for fillwith
 --------------------------------------------------------------------------------
 os.setenv ("PATH", "/usr/local/texlive/bin:/usr/bin:")
@@ -6,54 +6,10 @@ os.setenv ("TEXMFHOME", ".")
 os.setenv ("TEXMFLOCAL", ".")
 os.setenv ("TEXMFARCH", ".")
 --------------------------------------------------------------------------------
-function update_tag (file,content,tagname,tagdate)
-  -- stolen from l2e build-config.lua
-  local year = os.date("%Y")
-  local dyddiad = os.date("%Y-%m-%d")
-  if string.match(content,"%%+ +[ a-zA-Z0-9]* [Cc]opyright %([Cc]%) %d%d%d%d-%d%d%d%d Clea F%. Rees") then
-    content = string.gsub(content,
-    "[cC]opyright %([cC]%) (%d%d%d%d)%-%d%d%d%d Clea F%. Rees",
-    "Copyright (C) %1-" .. year .. " Clea F. Rees")
-  elseif string.match(content,"%%+ +[ a-zA-Z0-9]* [cC]opyright %([cC]%) %d%d%d%d Clea F%. Rees") then
-    local oldyear = string.match(content,"%%+ +[a-zA-Z0-9 ]* [cC]opyright %([cC]%) (%d%d%d%d) Clea F%. Rees")
-    if not year ~= oldyear then
-      content = string.gsub(content,
-      "[cC]opyright %([cC]%) %d%d%d%d Clea F%. Rees",
-      "Copyright (C) " .. oldyear .. "-" .. year .. " Clea F. Rees")
-    end
-  end
-  if string.match (file,"%.ins$") or string.match (file,"%.txt$") or string.match (file,"%.md$")  then
-    if string.match(content,"^[Cc]opyright %([cC]%) %d%d%d%d-%d%d%d%d Clea F%. Rees\n") then
-      content = string.gsub(content,
-      "[cC]opyright %([cC]%) (%d%d%d%d)%-%d%d%d%d Clea F%. Rees\n",
-      "Copyright (C) %1-" .. year .. " Clea F. Rees\n")
-    elseif string.match(content,"[cC]opyright %([cC]%) %d%d%d%d Clea F%. Rees\n") then
-      local oldyear = string.match(content,"[cC]opyright %([cC]%) (%d%d%d%d) Clea F%. Rees\n")
-      if not year ~= oldyear then
-        content = string.gsub(content,
-        "[cC]opyright %([cC]%) %d%d%d%d Clea F%. Rees\n",
-        "Copyright (C) " .. oldyear .. "-" .. year .. " Clea F. Rees\n")
-      end
-    end
-  end
-  local vtagname = string.gsub(tagname, "^v*(%d)", "v%1")
-  tagname = string.gsub(tagname, "^v*(%d)", "%1")
-  if string.match (file,"%.dtx$") or string.match (file,"%.ins") then
-    return string.gsub (content,
-    "(\\ProvidesFileSVN%{%$[^%}]*%$%} *%[)v%d[%d%.]*( *\\revinfo%])",
-    "%1" .. vtagname .. "%2")
-  elseif string.match (file,"%.md$") or string.match (file, "README*") then
-    if string.match (content,"\nVersion %d[%d%.]* *\n") then
-      return string.gsub (content,
-      "(\nClea F%. Rees *\nVersion )%d[%.%d]* *\n%d%d%d*[%/%-]%d%d%d*[%/%-]%d%d%d* *(\n)",
-      "%1" .. tagname .. "\n" .. dyddiad .. "%2")
-    else return string.gsub (content,
-      "(\nClea F%. Rees *\n)%d%d%d*[%/%-]%d%d%d*[%/%-]%d%d%d* *(\n)",
-      "%1Version " .. tagname .. "\n" .. dyddiad .. "%2")
-    end
-  end
-  return content
-end
+local maindir = maindir or "."
+local l3dir = maindir .. "/../adnoddau/l3build"
+--------------------------------------------------------------------------------
+dofile(l3dir .. "/tag.lua")
 --------------------------------------------------------------------------------
 ctanpkg = "fillwith"
 module = "fillwith"
@@ -74,4 +30,5 @@ uploadconfig = {
   topic = {"exercise", "teaching", "leaders", "expl3"}, 
 }
 --------------------------------------------------------------------------------
+dofile(l3dir .. "/manifest.lua")
 -- vim: ts=2:sw=2:
